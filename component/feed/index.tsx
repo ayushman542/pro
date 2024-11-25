0652f20fa2e1442dd22a66c74a5fa9f38fed132c
ayushman medcalia <ayushmanmedcalia089@gmail.com>
2024-11-25 15:07:28 +0530

    Done

---
+++ component/feed/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Define the FeedCard component
+const FeedCard: React.FC = () => {
+  return (
+    <div>
+      Hello jaan
+    </div>
+  );
+};
+
+export default FeedCard;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['ss.com'], // Replace with the actual domain where images are hosted
+  },
 };
 
 export default nextConfig;
